42fab56db1398a8189530e5fba0836d9b1f9ff61
signsofdidier <signsofdidier@gmail.com>
2025-05-26 22:12:52 +0200

    fail fail fail

--- app/Multitenancy/Tasks/PrefixTableNamesTask.php
+++ app/Multitenancy/Tasks/PrefixTableNamesTask.php
@@ -3,18 +3,20 @@
 namespace App\Multitenancy\Tasks;
 
 use Illuminate\Support\Facades\DB;
-use Spatie\Multitenancy\Contracts\Tenant;
+use Spatie\Multitenancy\Models\Tenant;
 use Spatie\Multitenancy\Tasks\SwitchTenantTask;
 
 class PrefixTableNamesTask implements SwitchTenantTask
 {
     public function makeCurrent(Tenant $tenant): void
     {
-        DB::connection()->setTablePrefix('tenant_' . $tenant->id . '_');
+        config(['database.connections.tenant.prefix' => 'tenant_']);
+        DB::purge('tenant');
     }
 
     public function forgetCurrent(): void
     {
-        DB::connection()->setTablePrefix('');
+        config(['database.connections.tenant.prefix' => null]);
+        DB::purge('tenant');
     }
 }
